1fdc0a21ef723575e269f42e00662721f6977e14
Jack Herrington <jherr@pobox.com>
2021-01-18 17:44:05 -0800

    First commit

--- README.md
+++ README.md
@@ -1,46 +1,6 @@
-# Getting Started with Create React App
+# Hackin' the AWS UI React toolkit
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is an experiment in seeing what you can do in an hour with a new UI toolkit that has no officially released documentation and that doesn't have a link to a Github page for the source.
 
-## Available Scripts
-
-In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+* [Article on the toolkit](https://www.theregister.com/AMP/2021/01/18/aws_creating_new_open_source/)
+* [NPM library page for the toolkit](https://www.npmjs.com/package/@awsui/components-react)
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,161 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { proxy, useProxy } from "valtio";
+
+import "./App.css";
+import pokemon from "./pokemon.json";
+
+import {
+  AppLayout,
+  Input,
+  Cards,
+  ColumnLayout,
+  Box,
+  Button,
+  Table,
+} from "@awsui/components-react";
+
+interface Pokemon {
+  id: number;
+  name: {
+    english: string;
+    japanese: string;
+    chinese: string;
+    french: string;
+  };
+  type: string[];
+  base: {
+    HP: number;
+    Attack: number;
+    Defense: number;
+    "Sp. Attack": number;
+    "Sp. Defense": number;
+    Speed: number;
+  };
+}
+
+interface IStore {
+  pokemon: Pokemon[];
+  text: string;
+  selectedPokemon: Pokemon | null;
+}
+
+const store = proxy<IStore>({
+  pokemon,
+  text: "",
+  selectedPokemon: null,
+});
+
+interface PokemonImageProps {
+  pokemon: Pokemon;
+}
+const PokemonImage = ({ pokemon }: PokemonImageProps) => (
+  <img
+    src={`/pokemon/${pokemon.name.english}.jpg`}
+    alt={pokemon.name.english}
+    style={{
+      maxWidth: "100%",
+    }}
+  />
+);
+
+const Content = () => {
+  const snapshot = useProxy(store);
+
+  const pokemonSlice = snapshot.pokemon
+    .filter((p) => p.name.english.includes(snapshot.text))
+    .slice(0, 15);
+
+  return (
+    <ColumnLayout>
+      <Input
+        value={snapshot.text}
+        onChange={(evt) => (store.text = evt.detail.value)}
+      />
+      <Cards
+        cardsPerRow={[
+          { cards: 3, minWidth: 600 },
+          { cards: 5, minWidth: 800 },
+        ]}
+        items={pokemonSlice}
+        cardDefinition={{
+          header: (item) => item.name.english,
+          sections: [
+            {
+              content: (item) => (
+                <Box fontSize="body-s">{item.name.english}</Box>
+              ),
+            },
+            {
+              content: (item) => <PokemonImage pokemon={item} />,
+            },
+            {
+              content: (item) => (
+                <Button
+                  variant="link"
+                  onClick={() => (store.selectedPokemon = item)}
+                >
+                  <Box fontSize="body-s">More Info...</Box>
+                </Button>
+              ),
+            },
+          ],
+        }}
+      />
+    </ColumnLayout>
+  );
+};
+
+const SelectedPokemon = () => {
+  const snapshot = useProxy(store);
+
+  const base: { [key: string]: string | number } =
+    snapshot.selectedPokemon?.base || {};
+  const baseItems = Object.keys(base).map((key) => ({
+    key,
+    value: base[key].toString(),
+  }));
+
+  return (
+    <Box
+      margin={{
+        left: "m",
+        right: "m",
+      }}
+    >
+      <h1>{snapshot.selectedPokemon?.name.english}</h1>
+      <PokemonImage pokemon={snapshot.selectedPokemon as Pokemon} />
+      <Table
+        items={baseItems}
+        columnDefinitions={[
+          {
+            header: "Name",
+            cell: (item) => (
+              <Box fontSize="body-s" fontWeight="bold">
+                {item.key}
+              </Box>
+            ),
+          },
+          {
+            header: "Value",
+            cell: (item) => <Box fontSize="body-s">{item.value}</Box>,
+          },
+        ]}
+      />
+    </Box>
+  );
+};
 
 function App() {
+  const snapshot = useProxy(store);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppLayout
+      navigationOpen={false}
+      content={<Content />}
+      toolsWidth={400}
+      toolsOpen={snapshot.selectedPokemon !== null}
+      tools={snapshot.selectedPokemon ? <SelectedPokemon /> : null}
+    />
   );
 }
 
